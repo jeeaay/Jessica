eaf5dd01d836c1caf965620d79ae88e9e2d365bf
jeay <wrjie@msn.cn>
2017-06-23 17:10:43 +0800

    配置模板

---
+++ seo/config.simple.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * @Author: Jeay 
+ * @Date: 2017-06-23 16:06:27 
+ * @Last Modified time: 2017-06-23 16:06:27 
+ */
+$config = [
+    //网站标题
+    "webTitle" => "",
+    //网站栏目名
+    "cateTitle" => [],
+    //首页关键词
+    "indexKeyword" => "",
+    //首页描述
+    "indexDescription" => "",
+    //是否在url中显示标题
+    "urlTitle" => "",
+    //是否在标题后面添加随机关键词
+    "keywordFileSwitch" => "",
+    //储存随机关键词的文件名
+    "keywordFilesName" => "",
+];
