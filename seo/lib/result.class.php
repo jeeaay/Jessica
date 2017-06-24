3f5aef6ece25d0fa0985f507cb270d5633066adc
jeay <wrjie@msn.cn>
2017-06-24 16:02:42 +0800

    栏目页

--- seo/lib/result.class.php
+++ seo/lib/result.class.php
@@ -3,9 +3,9 @@
  * @Author: Jeay 
  * @Date: 2017-06-24 12:44:49 
  * @Last Modified by: Jeay
- * @Last Modified time: 2017-06-24 14:51:20
+ * @Last Modified time: 2017-06-24 15:56:53
  */
-class Result extends Temp
+class Result extends SQLite
 {
     private $config;
     private $type;
@@ -14,23 +14,39 @@ class Result extends Temp
         $this->config = $config;
         $this->type = $type;
     }
+    private function cateList(String $dbName = "", Int $page = 0, Int $postsNum = 20)
+    {
+        if (is_numeric($this->config["postsNum"])) {
+            $postsNum = $this->config["postsNum"];
+        }
+        if ($dbName=="" || !is_file($dbName)) {
+            return 404;
+        }
+        try{
+            $this->connection=new PDO('sqlite:'.$dbName);
+        }catch(PDOException $e){
+            exit ("数据库错误！".$e);
+        }
+        $sql = 'select * from Content order by id desc limit '.$postsNum.' offset '.$page*$postsNum;
+        return $this->getlist($sql);
+    }
     public function GetContent()
     {
         switch ($this->type["type"]) {
             case 'index':
-                $this->GetIndex();
+                return $this->GetIndex();
                 break;
             
             case 'sitemap':
-                $this->GetSitemap();
+                return $this->GetSitemap();
                 break;
             
             case 'category':
-                $this->GetCategory();
+                return $this->GetCategory();
                 break;
             
             case 'single':
-                $this->GetSingle();
+                return $this->GetSingle();
                 break;
             
             default:
@@ -49,11 +65,12 @@ class Result extends Temp
     }
     private function GetCategory()
     {
-        $cont = [];
-        foreach ($this->config["cateTitle"] as $key => $value) {
-            $cont[$key]["url"] = "/".str_replace(" ","-",$value)."/";
-            $cont[$key]["cateTitle"] = $value;
+        $dbName = WEBROOT."/data/".str_replace("-"," ",urldecode($this->type["cate"])).".db";
+        if($list = $this->cateList($dbName,$this->type["page"]) == "404"){
+            return 404;
+        }else{
+            require TMPPATH."/".$this->config["tempName"]."/category.html";
         }
-        require TMPPATH."/".$this->config["tempName"]."/index.html";
+        exit;
     }
 }
