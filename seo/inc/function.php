bf105ae5fdab589ab0cc110132fd41a4ee1261eb
Jeay <wrjie@msn.cn>
2017-06-26 00:56:46 +0800

    通用方法

--- seo/inc/function.php
+++ seo/inc/function.php
@@ -3,15 +3,16 @@
  * @Author: Jeay 
  * @Date: 2017-06-25 23:48:28 
  * @Last Modified by: Jeay
- * @Last Modified time: 2017-06-25 23:49:32
+ * @Last Modified time: 2017-06-26 00:29:51
  */
  /*
  */
  function NotFound(String $msg = "Page Not Found")
     {
+        Global $config;
         header("HTTP/1.1 404 Not Found");
-        if (file_exists(TMPPATH."/".$this->config["tempName"]."/404.html")) {
-            include TMPPATH."/".$this->config["tempName"]."/404.html";
+        if (file_exists(TMPPATH."/".$config["tempName"]."/404.html")) {
+            include TMPPATH."/".$config["tempName"]."/404.html";
         }else{
             include CMSPATH."/inc/404.php";
         }
