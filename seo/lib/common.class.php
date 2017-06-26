08844e9064951905d536393ad1f94a594702bcae
jeay <wrjie@msn.cn>
2017-06-26 09:52:31 +0800

    通用类

--- seo/lib/common.class.php
+++ seo/lib/common.class.php
@@ -5,19 +5,21 @@
  * @Last Modified by: Jeay
  * @Last Modified time: 2017-06-24 13:53:44
  */
-class Common 
+class Common
 {
     private $config;
-    function __construct(Array $config = [])
+    function __construct()
     {
+        global $config;
         $this->config = $config;
     }
     //显示404错误，优先使用模板中的404页面
-    public function NotFound(String $msg = "Page Not Found")
+    public static function NotFound()
     {
+        global $config;
         header("HTTP/1.1 404 Not Found");
-        if (file_exists(TMPPATH."/".$this->config["tempName"]."/404.html")) {
-            include TMPPATH."/".$this->config["tempName"]."/404.html";
+        if (file_exists(TMPPATH."/".$config["tempName"]."/404.html")) {
+            include TMPPATH."/".$config["tempName"]."/404.html";
         }else{
             include CMSPATH."/inc/404.php";
         }
